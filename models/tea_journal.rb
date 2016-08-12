4b1e3ae7adbd4c23fb3bde36643787d1294cd1cb
Kevin McGillivray <kevin.mcgillivray@me.com>
2016-08-12 12:13:56 -0500

    Write delete from csv method and update remove entry method

--- models/tea_journal.rb
+++ models/tea_journal.rb
@@ -20,19 +20,20 @@ class TeaJournal
   def add_entry(tea_name, type, brewing_method)
     new_entry = Entry.new(tea_name, type, brewing_method)
     entries << new_entry
-    write_to_csv(data_file, new_entry)
+    write_to_csv(new_entry)
   end
 
-  # def remove_entry(tea_name, type, brewing_method)
-  #
-  #   entries.each_with_index do |entry, index|
-  #     if entry.tea_name == tea_name && entry.type == type && entry.brewing_method == brewing_method
-  #       entries.delete(entry)
-  #     end
-  #   end
-  #
-  # end
-  #
+  def remove_entry(tea_name, type, brewing_method)
+
+    entries.each_with_index do |entry, index|
+      if entry.tea_name == tea_name && entry.type == type && entry.brewing_method == brewing_method
+        entries.delete(entry)
+        delete_from_csv(entry)
+      end
+    end
+
+  end
+
   def import_from_csv(file_name)
     csv_text = File.read(file_name)
     csv = CSV.parse(csv_text, headers: true, skip_blanks: true)
@@ -50,9 +51,24 @@ class TeaJournal
     results
   end
 
-  def write_to_csv(file_name, entry)
-    CSV.open(file_name, "a+") do |csv|
+  def write_to_csv(entry)
+    CSV.open(data_file, "a+") do |csv|
       csv << [entry.tea_name, entry.type, entry.brewing_method]
+    end
+  end
+
+  def delete_from_csv(entry)
+    csv_array = CSV.read(data_file)
+    csv_array.each do |row|
+      if row[0] == entry.tea_name && row[1] == entry.type && row[2] == entry.brewing_method
+        csv_array.delete(row)
+      end
+    end
+
+    CSV.open(data_file, 'w') do |csv|
+      csv_array.each do |row|
+        csv << row
+      end
     end
   end
 end
